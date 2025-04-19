e2fadf767ab9730700754b6625ef3afa2845747d
Praydchicken <marksamonte0924@gmail.com>
2025-04-19 12:39:29 +0800

    Remove unused imports and layout definition from ClassRoom.vue for cleaner code

--- resources/js/Pages/AdminDashboard/ClassRoom.vue
+++ resources/js/Pages/AdminDashboard/ClassRoom.vue
@@ -1,14 +1,6 @@
 <script setup>
-import DashboardLayout from '../../components/AdminDashboardLayout.vue';
-import { Head, useForm } from '@inertiajs/vue3';
 import { ref } from 'vue';
 import { router } from '@inertiajs/vue3';
-
-
-defineOptions({
-  layout: DashboardLayout
-});
-
 </script>
 
 <template>
