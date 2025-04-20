a7a041df42731a90f7a297f46cf04c2b7093857d
Praydchicken <marksamonte0924@gmail.com>
2025-04-20 09:06:04 +0800

    Fix duplicate entry in admin links for StudentDashboardLayout

--- resources/js/components/StudentDashboardLayout.vue
+++ resources/js/components/StudentDashboardLayout.vue
@@ -16,8 +16,8 @@ const adminLinks = [
   { name: "Dashboard", url: route('student.dashboard')},
   { name: "Profile Information", url: '#' },
   { name: "Schedule", url: route('student.schedule') },
-  { name: "Schedule", url: route('student.view.schedule') },
-  { name: "Grade Report", url: '#' },
+  { name:"View Balance", url: route('student.balances') }
+  // { name: "Grade Report", url: '#' },
 ];
 
 const headerContent = computed(() => [
