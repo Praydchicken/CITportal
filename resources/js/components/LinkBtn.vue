86ee35e5c2c0c2f828c0fe69bbdfbc6372f4abb4
Praydchicken <marksamonte0924@gmail.com>
2025-05-04 21:30:24 +0800

    Add LinkBtn component for dynamic routing and labeling

---
+++ resources/js/components/LinkBtn.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+	routeName: String,
+	label: String,
+});
+</script>
+<template>
+
+<Link :href="route(routeName)" class="inline-block bg-yellow-600 text-white px-3 py-1 rounded hover:bg-yellow-800">
+    {{ label }}
+  </Link>
+</template>
